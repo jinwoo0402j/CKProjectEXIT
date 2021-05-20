4424f157c5e2932dd0f55581a3ce128c12f97259
JUNG JISANG <ashuatz@gmail.com>
2021-05-20 20:17:58 +0900

    *replace 누락커밋

---
+++ Assets/Scripts/ScriptableObject/BasePatternConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+[CreateAssetMenu(fileName = "NewPatternData", menuName = "PatternData", order = 0)]
+#endif
+
+[Serializable]
+public class BasePatternConfig : ScriptableObject
+{
+    [Space(20)]
+
+    [SerializeField]
+    private GameObject bullet_origin;
+
+    [SerializeField]
+    private float pattern_delay;
+
+    public GameObject BULLET_ORIGIN { get => bullet_origin; }
+    public float PATTERN_DELAY { get => pattern_delay; }
+
+    public virtual IEnumerator Run() 
+    {
+        float startTime = Time.time;
+        yield break; 
+    }
+}
---
+++ Assets/Scripts/ScriptableObject/BossConfig.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+[CreateAssetMenu(fileName = "NewBossData", menuName = "BossData", order = 0)]
+#endif
+
+[Serializable]
+public class BossConfig : ScriptableObject
+{
+    [Space(20)]
+
+
+    [SerializeField]
+    private int attack_damage;
+
+    [Header("range : 0 - 1")]
+    [SerializeField]
+    private List<float> phase_rate;
+
+    [SerializeField]
+    private int default_hp;
+
+    [Header("unit : second")]
+    [SerializeField]
+    private float attack_delay;
+
+    [Header("unit : m/s")]
+    [SerializeField]
+    private float bullet_speed;
+
+
+    public int ATTACK_DAMAGE { get => attack_damage; }
+    public int DEFAULT_HP { get => default_hp; }
+    public float ATTACK_DELAY { get => attack_delay; }
+    public float BULLET_SPEED { get => bullet_speed; }
+    public List<float> PHASE_RATE { get => phase_rate.ToList(); }
+}

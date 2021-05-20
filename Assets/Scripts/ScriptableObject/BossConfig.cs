9eb4aeadf1b88848bcb1378c5e68c85b66992c22
JUNG JISANG <ashuatz@gmail.com>
2021-05-20 21:13:32 +0900

    * 제어값 밖으로 빼기

--- Assets/Scripts/ScriptableObject/BossConfig.cs
+++ Assets/Scripts/ScriptableObject/BossConfig.cs
@@ -28,7 +28,22 @@ public class BossConfig : ScriptableObject
 
     [Header("unit : second")]
     [SerializeField]
-    private float attack_delay;
+    private float melee_attack_charge_delay;
+    [Header("unit : second")]
+    [SerializeField]
+    private float melee_attack_delay;
+
+    [Header("unit : int")]
+    [SerializeField]
+    private int melee_attack_damage;
+
+    [Header("unit : meter")]
+    [SerializeField]
+    private float melee_attack_threadhold;
+
+    [Header("unit : meter")]
+    [SerializeField]
+    private float melee_attack_range;
 
     [Header("unit : m/s")]
     [SerializeField]
@@ -37,7 +52,11 @@ public class BossConfig : ScriptableObject
 
     public int ATTACK_DAMAGE { get => attack_damage; }
     public int DEFAULT_HP { get => default_hp; }
-    public float ATTACK_DELAY { get => attack_delay; }
+    public float MELEE_ATTACK_CHARGE_DELAY { get => melee_attack_charge_delay; }
+    public float MELEE_ATTACK_DELAY { get => melee_attack_delay; }
+    public int MELEE_ATTACK_DAMAGE { get => melee_attack_damage; }
+    public float MELEE_ATTACK_THREADHOLD { get => melee_attack_threadhold; }
+    public float MELEE_ATTACK_RANGE { get => melee_attack_range; }
     public float BULLET_SPEED { get => bullet_speed; }
     public List<float> PHASE_RATE { get => phase_rate.ToList(); }
 }

17b4e44f3627b61d8decf683b10e11c497185fdc
sahaga <tkfkrk123@gmail.com>
2021-07-13 22:44:32 +0900

    Main02

---
+++ Assets/Scripts/Entities/Wall.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject Boss;
+
+    public float Boss_hp;
+    public float Dissolve_C;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Wall_hp();
+    }
+
+    void Wall_hp()
+    {
+        Boss_hp = Boss.GetComponent<TestBoss>().HP.CurrentData;
+        Dissolve_C = Boss_hp / 100 - 0.7f;
+        this.GetComponent<Renderer>().material.SetFloat("Vector1_64f45ecfc44240519da5a9970de70f2b", Dissolve_C);
+    }
+}
